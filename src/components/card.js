72967beedbcbbbf13e32c6e5beab12a0157f5e96
wachirawitpituckwanich <wachirawitpi@kkumail.com>
2021-11-29 22:31:54 +0700

    fetch api test

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import './card.css'
-import celts from './Celts.png' 
+import axios from 'axios'
+
 const Carditem = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() =>{
+        const loadData = async () => {
+            setLoading(true);
+            const response = await axios.get('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
+            setData(response.data.civilizations);
+            setLoading(false);
+        }
+        loadData();
+    }, [])
     return(
         <div className="card">
             <div className="card-img">
-                <img src={celts} alt="civ-emblem"></img>
+                {loading ? (<h1>Loading...</h1>) : (data.map(item => <p>{item.name}</p>))}
             </div>
         </div>
         
     )
 }
 export default Carditem;
+/**{data.map((value,key) => {
+    return(
+        <p>{value}</p>
+    );
+})}*/
