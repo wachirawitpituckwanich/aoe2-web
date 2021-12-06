2f72ebc84a12a826f62c4823e8668dc24062519c
wachirawitpituckwanich <wachirawitpi@kkumail.com>
2021-12-06 20:01:49 +0700

    cors test

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -10,7 +10,7 @@ const Searchbar = () => {
     const loadData = async () => {
       setLoading(true);
       const response = await axios.get(
-        "/civilizations"
+        "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
       );
       const res = response.data.civilizations
       console.log(res);
--- src/components/civdetails.js
+++ src/components/civdetails.js
@@ -16,7 +16,7 @@ const CivDetails = ({ match }) => {
     const loadData = async () => {
       setLoading(true);
       const response = await axios.get(
-        "/civilizations"
+        "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
       );
       const res = response.data.civilizations
       // remove duplicates from response
